5bd08fef33123ff8d1bf5b1fc84ae2a38cbf14d5
Semjon Wilke <1882648+SemjonWilke@users.noreply.github.com>
2022-09-02 14:40:44 +0200

    Update Robot_Reboot_Hotkeys.js

--- Robot_Reboot_Hotkeys.js
+++ Robot_Reboot_Hotkeys.js
@@ -6,36 +6,29 @@
 // ==/UserScript==
 
 // https://stackoverflow.com/questions/6157929/how-to-simulate-a-mouse-click-using-javascript
-function simulate(element, eventName)
-{
+function simulate(element, eventName) {
     var options = extend(defaultOptions, arguments[2] || {});
     var oEvent, eventType = null;
 
-    for (var name in eventMatchers)
-    {
+    for (var name in eventMatchers) {
         if (eventMatchers[name].test(eventName)) { eventType = name; break; }
     }
 
-    if (!eventType)
-        throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+    if (!eventType) throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
 
-    if (document.createEvent)
-    {
+    if (document.createEvent) {
         oEvent = document.createEvent(eventType);
-        if (eventType == 'HTMLEvents')
-        {
+        if (eventType == 'HTMLEvents') {
             oEvent.initEvent(eventName, options.bubbles, options.cancelable);
         }
-        else
-        {
+        else {
             oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
             options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
             options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
         }
         element.dispatchEvent(oEvent);
     }
-    else
-    {
+    else {
         options.clientX = options.pointerX;
         options.clientY = options.pointerY;
         var evt = document.createEventObject();
@@ -46,8 +39,7 @@ function simulate(element, eventName)
 }
 
 function extend(destination, source) {
-    for (var property in source)
-      destination[property] = source[property];
+    for (var property in source) destination[property] = source[property];
     return destination;
 }
 
@@ -67,13 +59,33 @@ var defaultOptions = {
     cancelable: true
 }
 
-records = document.getElementsByClassName('flex-box robots-container flex-wrap')[0].getElementsByClassName('record')
+playmode = document.getElementsByClassName('play-mode')
+playmode[0].style.display="flex"
+console.log(playmode[0])
+playmode[0].firstChild.style.margin="auto"
 
-document.addEventListener('keydown', (e) => {
-  if (e.code === "Digit1") simulate(records[0], "click")
-  else if (e.code === "Digit2") simulate(records[1], "click")
-  else if (e.code === "Digit3") simulate(records[2], "click")
-  else if (e.code === "Digit4") simulate(records[3], "click")
-  else if (e.code === "Digit5") simulate(records[4], "click")
-  else if (e.code === "Escape") simulate(document.getElementsByClassName("redo fa fa-refresh")[0], "click")
-});
+window.addEventListener('load', function() {
+    records = document.getElementsByClassName('flex-box robots-container flex-wrap')[0].getElementsByClassName('record')
+    robots = document.getElementsByClassName('robot')
+    reset = document.getElementsByClassName("redo fa fa-refresh")[0]
+  
+    document.addEventListener('keydown', (e) => {
+        if (e.code === "Digit1") simulate(records[0], "click")
+        else if (e.code === "Digit2") simulate(records[1], "click")
+        else if (e.code === "Digit3") simulate(records[2], "click")
+        else if (e.code === "Digit4") simulate(records[3], "click")
+        else if (e.code === "Digit5") simulate(records[4], "click")
+        else if (e.code === "Escape") simulate(reset, "click")
+        else if (e.code === "KeyA") simulate(robots[0], "click")
+        else if (e.code === "KeyS") simulate(robots[1], "click")
+        else if (e.code === "KeyD") simulate(robots[2], "click")
+        else if (e.code === "KeyW") simulate(robots[3], "click")
+    });
+
+    for (var i = 0; i < 5; i++) {
+        var newdiv = document.createElement("div")    
+        newdiv.Class = "recordnumber"
+        newdiv.innerHTML = "<center>"+(i+1)+"</center>"
+        records[i].appendChild(newdiv)
+    }
+}, false);
